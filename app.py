5ffe0f75fd6b2edd213cd7763c1fbe263c13d533
fahadcr14 <bsdsf21m014@pucit.edu.pk>
2024-12-19 06:16:19 +0500

    v2

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from flask import Flask, render_template, request, jsonify
 from werkzeug.utils import secure_filename
 import torch
@@ -13,7 +14,22 @@ app = Flask(__name__)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Model and transformation setup
+def download_model_if_not_exists(url, model_path):
+    """Download model from Hugging Face repository if it doesn't exist locally."""
+    if not os.path.exists(model_path):
+        print("Model not found locally, downloading from Hugging Face...")
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open(model_path, 'wb') as f:
+                f.write(response.content)
+            print(f"Model downloaded and saved to {model_path}")
+        else:
+            print("Failed to download model. Please check the URL.")
+    else:
+        print("Model already exists locally.")
+
 def load_model(model_path):
+    """Load model from the given path."""
     model = torch.load(model_path, map_location=torch.device('cpu'))
     model.eval()  # Set model to evaluation mode
     model.to(device)
@@ -29,12 +45,13 @@ def preprocess_image(image_path):
     return transform(image).unsqueeze(0)  # Add batch dimension
 
 def get_probabilities(logits):
-    # Apply softmax to get probabilities
+    """Apply softmax to get probabilities."""
     probabilities = F.softmax(logits, dim=1)
     percentages = probabilities * 100
     return percentages
 
 def predict(image_path, model, class_names):
+    """Make prediction using the trained model."""
     image_tensor = preprocess_image(image_path).to(device)
     model.eval()
     with torch.inference_mode():  # Disable gradient calculations
@@ -47,8 +64,14 @@ def predict(image_path, model, class_names):
 # Define class names
 class_names = ['Heart', 'Oblong', 'Oval', 'Round', 'Square']
 
-# Load model
+# Path to the model file
 model_path = r"model_85_nn_.pth"  # Update this with the correct model path
+model_url = "https://huggingface.co/fahd9999/model_85_nn_/resolve/main/model_85_nn_.pth?download=true"
+
+# Download the model only if it doesn't exist locally
+download_model_if_not_exists(model_url, model_path)
+
+# Load the model
 model = load_model(model_path)
 
 # API to render the index page
